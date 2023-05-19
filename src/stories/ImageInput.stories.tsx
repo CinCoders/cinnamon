d40d1d67db888881f8cb22622ecf7845fafa19d6
Yuri Correia de Barros <ycb@cin.ufpe.br>
2023-05-19 11:08:39 -0300

    <fix> license information
    <fix> license information updated

--- src/stories/ImageInput.stories.tsx
+++ src/stories/ImageInput.stories.tsx
@@ -10,8 +10,6 @@ export default {
 export const Default = () => {
   const [file, setFile] = useState<File>(new File([''], 'filename'));
 
-  console.log(file);
-
   return (
     <div style={{ width: '10rem', padding: '1rem' }}>
       <ImageInput setFile={setFile} id='image-input-test' />
