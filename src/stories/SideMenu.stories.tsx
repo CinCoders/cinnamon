961ce503992eb980dd647675ba6e7db086e6aa2a
Lucas Daniel Costa Da Silva <ldcs@cin.ufpe.br>
2023-06-02 13:23:51 -0300

    <fix> fix sideMenu story

--- src/stories/SideMenu.stories.tsx
+++ src/stories/SideMenu.stories.tsx
@@ -1,72 +1,84 @@
 import { useState } from 'react';
 import { SideMenu } from '../components/SideMenu';
-import { SideMenuLink } from '@/interfaces';
-import HomeIcon from '@mui/icons-material/Home';
-import DescriptionIcon from '@mui/icons-material/Description';
 import { BrowserRouter } from 'react-router-dom';
+import { Meta, StoryFn } from '@storybook/react';
+import { testLinks } from './sampledata/SampleData';
+import { SideMenuLink } from '../interfaces';
+import { Button } from '@mui/material';
 
 export default {
   title: 'Components/SideMenu',
-  component: SideMenu
-};
-
-const HomeIconComponent = () => {
-  return <HomeIcon />;
-};
-const DescriptionIconComponent = () => {
-  return <DescriptionIcon />;
-};
-
-export const Default = () => {
-  const testLinks: SideMenuLink[] = [
-    {
-      title: 'Homepage',
-      href: '#',
-      IconComponent: HomeIconComponent
-    },
-    {
-      title: 'Process',
-      href: '#',
-      IconComponent: DescriptionIconComponent,
-      children: [
-        {
-          title: 'Register/Edit',
-          href: '#'
-        },
-        {
-          title: 'List',
-          href: '#'
-        }
-      ]
-    },
-    {
-      title: 'Types',
-      href: '#',
-      IconComponent: DescriptionIconComponent,
-      children: [
-        {
-          title: 'Register/Edit',
-          href: '#'
-        },
-        {
-          title: 'List',
-          href: '#'
-        }
-      ]
+  component: SideMenu,
+  parameters: {
+    docs: {
+      page: null
     }
-  ];
+  },
+  argTypes: {
+    visibility: {
+      name: 'visibility',
+      control: { disable: true },
+      description: 'Boolean which defines the visibility of sidemenu.',
+      table: {
+        category: 'SideMenu'
+      }
+    },
+    setVisibility: {
+      name: 'setVisibility',
+      control: { disable: true },
+      description:
+        'useState set function passed to define the visibility of sidemenu in actions.',
+      table: {
+        category: 'SideMenu'
+      }
+    },
+    links: {
+      name: 'links',
+      description: 'Array of links which defines the custom side menu',
+      table: {
+        category: 'SideMenu'
+      }
+    }
+  }
+} as Meta;
 
-  const [drawerVisibility, setDrawerVisibility] = useState(true);
+interface SideMenuStoryProps {
+  links: SideMenuLink[];
+}
+
+const Template: StoryFn<SideMenuStoryProps> = (args) => {
+  const [drawerVisibility, setDrawerVisibility] = useState<boolean>(false);
 
   return (
-    <div style={{ margin: '10px' }}>
-      <BrowserRouter>
-        <SideMenu
-          visibility={drawerVisibility}
-          setVisibility={setDrawerVisibility}
-          links={testLinks}
-        />
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div
+        style={{
+          width: '100%',
+          height: '64px',
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center'
+        }}
+      >
+        <Button
+          type='button'
+          variant='contained'
+          onClick={() => setDrawerVisibility(!drawerVisibility)}
+        >
+          {drawerVisibility ? 'Close SideMenu' : 'Open SideMenu'}
+        </Button>
+      </div>
+
+      <SideMenu
+        visibility={drawerVisibility}
+        setVisibility={setDrawerVisibility}
+        links={args.links}
+      />
+    </BrowserRouter>
   );
+};
+
+export const SideMenu_ = Template.bind({});
+SideMenu_.args = {
+  links: testLinks
 };
