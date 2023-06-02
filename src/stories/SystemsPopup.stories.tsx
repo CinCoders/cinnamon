55e98494ef98b874f5780d742e642f185d9e192c
Lucas Daniel Costa Da Silva <ldcs@cin.ufpe.br>
2023-06-02 13:24:17 -0300

    <fix> fix systemPopup story

--- src/stories/SystemsPopup.stories.tsx
+++ src/stories/SystemsPopup.stories.tsx
@@ -1,12 +1,30 @@
 import { SystemsPopup } from '../components/SystemsPopup';
 import './storiesGlobals.css';
 import { testSystems } from './sampledata/SampleData';
+import { Meta, StoryFn } from '@storybook/react';
+import { System } from '..';
 
 export default {
   title: 'Components/SystemsPopup',
-  component: SystemsPopup
+  component: SystemsPopup,
+  argTypes: {
+    systemsList: {
+      name: 'systemsList',
+      control: 'object',
+      description: 'Array which defines the systems contained in popup'
+    }
+  }
+} as Meta;
+
+interface SystemsPopupProps {
+  systemsList: System[];
+}
+
+const Template: StoryFn<SystemsPopupProps> = ({ systemsList }) => {
+  return <SystemsPopup systemsList={systemsList} />;
 };
 
-export const Default = () => {
-  return <SystemsPopup systemsList={testSystems} />;
+export const SystemsList = Template.bind({});
+SystemsList.args = {
+  systemsList: testSystems
 };
