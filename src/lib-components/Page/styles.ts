34469adb5ad9f3798911c8932f99d1f593f48e01
Lucas Daniel Costa Da Silva <ldcs@cin.ufpe.br>
2023-08-03 16:52:53 -0300

    <fix> fix paddings to % instead of px

--- src/lib-components/Page/styles.ts
+++ src/lib-components/Page/styles.ts
@@ -2,10 +2,7 @@ import styled from 'styled-components';
 
 export const MainDiv = styled.main`
   display: flex;
-  padding-right: 40px;
-  padding-left: 40px;
-  padding-top: 20px;
-  padding-bottom: 20px;
+  padding: 20px clamp(10px, 2%, 40px);
   z-index: 1000;
   background-color: #ffffff;
 `;
