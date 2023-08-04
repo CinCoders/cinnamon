6c034f7ef3b794d1008ebfbd7652b507135647a9
Daniel Brandão <dcb@cin.ufpe.br>
2023-08-04 08:09:00 -0300

    Remove old auth utils

--- src/utils/keycloakUtils.ts
+++
@@ -1,9 +0,0 @@
-import Keycloak from 'keycloak-js';
-
-export function hasAccess(keycloak: Keycloak, roles: Array<string>) {
-  if (roles.includes('*')) return true;
-  for (const role of roles) {
-    if (keycloak.hasRealmRole(role)) return true;
-  }
-  return false;
-}
