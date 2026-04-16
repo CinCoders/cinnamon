2ff0c1646b4e4d34c4573735a6d9153305ff5df3
Gustavo Borges <ggb@cin.ufpe.br>
2026-04-16 02:26:27 -0300

    feat: expose hasAccess and CinnamonSession

--- src/entry-server.ts
+++ src/entry-server.ts
@@ -1,3 +1,5 @@
+export { hasAccess } from "./auth/hasAccess";
+export type { CinnamonSession } from "./auth/types";
 export { RequireAuthServer } from "./lib-components/RequireAuth/RequireAuthServer";
 export { PageWithAuthServer } from "./lib-components/PageWithAuth/PageWithAuthServer";
 export { PageServer } from "./lib-components/Page/PageServer";
